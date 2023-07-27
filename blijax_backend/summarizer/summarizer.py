f7353b3463bfe1e261addbbe58524305cf43162c
Aaron <aaron.nguy2008@gmail.com>
2023-07-27 14:48:27 -0700

    Added Django app

---
+++ blijax_backend/summarizer/summarizer.py
@@ -0,0 +1,31 @@
+# import library
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# configure openai to your account 
+openai.api_key = os.getenv("API_KEY")
+
+def summarize(text):
+	
+	# create prompt
+	prompt = "Write a concise summary of the following content: \n"
+	prompt += text
+	
+	# ping model and generate a response
+	response = openai.Completion.create(
+			engine = "text-davinci-003",
+			prompt = prompt,
+			
+	)
+	
+	# clean up response to just the actual String value and return 
+	answer = response["choices"][0]["text"].strip()
+	return answer 
+
+########################################################
+# you can tinker even further with model settings.     #
+# read about your options here                         #
+########################################################
