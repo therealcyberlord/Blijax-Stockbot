099aaf05c26b5f7b1a7a0afa6d3b62bd12e0549e
Sophia Lee <sophiadiddlybop@gmail.com>
2023-07-31 14:29:19 -0700

    got backbone of website page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-green-100 text-green-800">
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">Stock Dashboard</h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+         
+          <div className="bg-white rounded-lg p-4 shadow-md">
+          
+            <h2 className="text-lg font-semibold mb-2">Stock Index 1</h2>
+            <p className="text-gray-600">Current Value: $100</p>
+            <p className="text-gray-600">Change: +$5 (5%)</p>
+          </div>
+          <div className="bg-white rounded-lg p-4 shadow-md">
+        
+            <h2 className="text-lg font-semibold mb-2">Stock Index 2</h2>
+            <p className="text-gray-600">Current Value: $200</p>
+            <p className="text-gray-600">Change: -$10 (-5%)</p>
+          </div>
+          <div className="bg-white rounded-lg p-4 shadow-md">
+            
+            <h2 className="text-lg font-semibold mb-2">Stock Index 3</h2>
+            <p className="text-gray-600">Current Value: $300</p>
+            <p className="text-gray-600">Change: +$20 (7%)</p>
+          </div>
+          <div className="bg-white rounded-lg p-4 shadow-md">
+            
+            <h2 className="text-lg font-semibold mb-2">Stock Index 4</h2>
+            <p className="text-gray-600">Current Value: $400</p>
+            <p className="text-gray-600">Change: +$15 (3.5%)</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card text-blue-500">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
