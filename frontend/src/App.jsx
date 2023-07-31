085d87c2c867c0006bf4a229a94fa0be0c2b1c4b
Sophia Lee <sophiadiddlybop@gmail.com>
2023-07-31 13:49:55 -0700

    added tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card text-blue-500">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
