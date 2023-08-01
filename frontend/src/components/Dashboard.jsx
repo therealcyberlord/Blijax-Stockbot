f5c381a091e46215f433dd4efd52d232ab9edfe7
Sophia Lee <sophiadiddlybop@gmail.com>
2023-08-01 13:53:57 -0700

    Framework of website almost complete

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -7,19 +7,33 @@ const Dashboard = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div className="bg-gray-400 p-6 rounded-lg transform transition-all hover:scale-105">
           <h3 className="text-xl font-bold mb-2">Stocks and Futures</h3>
-          <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      
         </div>
         <div className="bg-gray-400 p-6 rounded-lg transform transition-all hover:scale-105">
           <h3 className="text-xl font-bold mb-2">Forex</h3>
-          <p className="text-gray-700">Sed id leo vel felis fermentum dignissim.</p>
+         
         </div>
         <div className="bg-gray-400 p-6 rounded-lg transform transition-all hover:scale-105">
           <h3 className="text-xl font-bold mb-2">Indices and Bonds</h3>
-          <p className="text-gray-700">Nulla porttitor fringilla blandit. Vivamus id aliquam turpis.</p>
+
+         </div>
+        <div className="bg-gray-400 p-6 rounded-lg transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">Live News Feed</h3>
+         
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">Live Market Data</h3> 
+          </div>
+        <div className="bg-gray-400 p-6 rounded-lg transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">Assets</h3>
+         
+        
         </div>
       </div>
     </div>
+
   );
-};
+ };
+ 
 
 export default Dashboard;
