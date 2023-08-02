966a4535c56d8a0fa093315cf61d1ae0ed2c463f
Xingyu Bian <xingyubian@umass.edu>
2023-08-02 16:14:55 -0400

    widget should fit in the container

--- frontend/src/components/Stockview.jsx
+++ frontend/src/components/Stockview.jsx
@@ -1,52 +1,65 @@
-// TradingViewWidget.jsx
-import React, { useEffect, useRef, memo } from 'react';
+import React, { useEffect, useRef } from "react";
 
-function TradingViewWidget() {
-  const container = useRef();
+let tvScriptLoadingPromise;
 
-  useEffect(
-    () => {
-      const script = document.createElement("script");
-      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.innerHTML = `
-        {
-          "symbols": [
-            [
-              "Apple",
-              "AAPL|1D"
-            ],
-            [
-              "Google",
-              "GOOGL|1D"
-            ],
-            [
-              "Microsoft",
-              "MSFT|1D"
-            ]
-          ],
-          "chartOnly": false,
-          "width": "100%", // Set width to 100% to fill the container
-          "height": 400,   // Set the desired height, e.g., 400px
-          // Rest of the configuration remains the same
-          // ...
-        }`;
-      container.current.appendChild(script);
-    },
-    []
-  );
+export default function TradingViewWidget() {
+  const onLoadScriptRef = useRef();
+
+  useEffect(() => {
+    onLoadScriptRef.current = createWidget;
+
+    if (!tvScriptLoadingPromise) {
+      tvScriptLoadingPromise = new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.id = "tradingview-widget-loading-script";
+        script.src = "https://s3.tradingview.com/tv.js";
+        script.type = "text/javascript";
+        script.onload = resolve;
+
+        document.head.appendChild(script);
+      });
+    }
+
+    tvScriptLoadingPromise.then(
+      () => onLoadScriptRef.current && onLoadScriptRef.current()
+    );
+
+    return () => (onLoadScriptRef.current = null);
+
+    function createWidget() {
+      if (
+        document.getElementById("tradingview_defcf") &&
+        "TradingView" in window
+      ) {
+        new window.TradingView.widget({
+          autosize: true,
+          symbol: "NASDAQ:AAPL",
+          interval: "D",
+          timezone: "Etc/UTC",
+          theme: "light",
+          style: "1",
+          locale: "en",
+          toolbar_bg: "#f1f3f6",
+          enable_publishing: false,
+          allow_symbol_change: true,
+          container_id: "tradingview_defcf",
+        });
+      }
+    }
+  }, []);
 
   return (
-    <div className="tradingview-widget-container" ref={container}>
-      <div className="tradingview-widget-container__widget"></div>
+    <div className="tradingview-widget-container">
+      <div id="tradingview_defcf" />
       <div className="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
+        <a
+          href="https://www.tradingview.com/"
+          rel="noopener nofollow"
+          target="_blank"
+        >
           <span className="blue-text">Track all markets on TradingView</span>
         </a>
       </div>
     </div>
   );
 }
-
-export default memo(TradingViewWidget);
