0701ecfa1449f28f8f79c63c58ed207e5aa45181
Jackson Pirooz <jacksonpirooz@Jacksons-MacBook-Air.local>
2023-08-02 13:40:24 -0700

    created text box and fixed the issue of stockbot not showing anything

--- frontend/src/pages/Stockbot.jsx
+++ frontend/src/pages/Stockbot.jsx
@@ -1,3 +1,17 @@
 const Stockbot = () => {
-     };
-  export default Stockbot;
+   return (
+     <div className="flex items-center justify-center h-screen">
+       <div className="fixed bottom-4 bg-gray-400 p-6 rounded-lg w-5/6">
+         <input
+           type="text"
+           className="w-full bg-white border border-gray-400 rounded px-4 py-2 mt-2"
+           placeholder="Type here..."
+         />
+       </div>
+     </div>
+   );
+ };
+ 
+ export default Stockbot;
+ 
+ 
