7e6269833f2192b77c6426b082171f0aef526ff0
Jackson Pirooz <jacksonpirooz@Jacksons-MacBook-Air.local>
2023-08-02 11:24:35 -0700

    created shadows in the about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,13 +1,27 @@
 const About = () => {
   return (
-    <section className="container mx-auto p-8">
-      <h2 className="text-4xl font-bold text-olive-green mb-4">About Me</h2>
-      <p className="text-gray-700 text-lg">
-        I am a passionate web developer with a strong background in front-end technologies.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor fringilla blandit.
-        Vivamus id aliquam turpis. Sed id leo vel felis fermentum dignissim.
-      </p>
-    </section>
+    <div className="container mx-auto mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">1</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">2</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">3</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">4</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">5</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">6</h3>
+        </div>
+      </div>
+    </div>
   );
 };
 
