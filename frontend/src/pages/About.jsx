14c6aa05f74eac78bba94b5b817241f4f69ecd25
Jackson Pirooz <jacksonpirooz@Jacksons-MacBook-Air.local>
2023-08-02 11:59:32 -0700

    created more shadows in the about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,23 +2,50 @@ const About = () => {
   return (
     <div className="container mx-auto mt-8">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-blue-500/50 transform transition-all hover:scale-105">
           <h3 className="text-xl font-bold mb-2">1</h3>
         </div>
-        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-indigo-500/50 transform transition-all hover:scale-105">
           <h3 className="text-xl font-bold mb-2">2</h3>
         </div>
-        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-stone-500/50 transform transition-all hover:scale-105">
           <h3 className="text-xl font-bold mb-2">3</h3>
         </div>
-        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-blue-500 transform transition-all hover:scale-105">
           <h3 className="text-xl font-bold mb-2">4</h3>
         </div>
-        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-indigo-500 transform transition-all hover:scale-105">
           <h3 className="text-xl font-bold mb-2">5</h3>
         </div>
-        <div className="bg-gray-400 p-6 rounded-lg shadow-md transform transition-all hover:scale-105">
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-stone-500 transform transition-all hover:scale-105">
           <h3 className="text-xl font-bold mb-2">6</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-emerald-500/50 transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">7</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-green-500/50 transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">8</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-amber-500/50 transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">9</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-emerald-500 transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">10</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-green-500 transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">11</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-amber-500 transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">12</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">13</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-xl transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">14</h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-2xl transform transition-all hover:scale-105">
+          <h3 className="text-xl font-bold mb-2">15</h3>
         </div>
       </div>
     </div>
