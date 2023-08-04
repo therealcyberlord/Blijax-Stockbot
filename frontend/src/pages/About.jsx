bdb9ba8123dcee1d027c2a6da037518571ac4877
Xingyu Bian <xingyubian@umass.edu>
2023-08-04 17:33:19 -0400

    updated about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,33 +1,59 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React from "react";
 
 const About = () => {
   return (
     <div className="container mx-auto mt-8">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-emerald-500 transform transition-all hover:scale-105">
-          <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 1" className="w-full mb-4" />
+          <img
+            src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+            alt="Image 6"
+            className="w-full mb-4"
+          />
+          <h3 className="text-xl font-bold mb-2">
+            Xingyu Bian - Product Manager
+          </h3>
+        </div>
+        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-emerald-500 transform transition-all hover:scale-105">
+          <img
+            src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+            alt="Image 1"
+            className="w-full mb-4"
+          />
           <h3 className="text-xl font-bold mb-2">Aaron Nguy - Backend</h3>
         </div>
         <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-emerald-500 transform transition-all hover:scale-105">
-          <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 2" className="w-full mb-4" />
+          <img
+            src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+            alt="Image 2"
+            className="w-full mb-4"
+          />
           <h3 className="text-xl font-bold mb-2">Blake Almon - Backend</h3>
         </div>
         <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-emerald-500 transform transition-all hover:scale-105">
-          <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 3" className="w-full mb-4" />
+          <img
+            src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+            alt="Image 3"
+            className="w-full mb-4"
+          />
           <h3 className="text-xl font-bold mb-2">Isaac Jung - Backend</h3>
         </div>
         <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-emerald-500 transform transition-all hover:scale-105">
-          <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 4" className="w-full mb-4" />
+          <img
+            src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+            alt="Image 4"
+            className="w-full mb-4"
+          />
           <h3 className="text-xl font-bold mb-2">Jackson Pirooz - Frontend</h3>
         </div>
         <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-emerald-500 transform transition-all hover:scale-105">
-          <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 5" className="w-full mb-4" />
+          <img
+            src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+            alt="Image 5"
+            className="w-full mb-4"
+          />
           <h3 className="text-xl font-bold mb-2">Lee Sophia - Frontend</h3>
-        </div>
-        <div className="bg-gray-400 p-6 rounded-lg shadow-lg shadow-emerald-500 transform transition-all hover:scale-105">
-          <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 6" className="w-full mb-4" />
-          <h3 className="text-xl font-bold mb-2">Xingyu Bian - Teacher</h3>
         </div>
       </div>
     </div>
