69888907c030079a4d10e9e49b5db1b266d5a68b
BlakeAlmon <all.business.blake@gmail.com>
2023-08-04 14:57:56 -0700

    What I have.
    For some reason I couldn't find the backend branch so here is the code I currently have.

--- blijax_backend/QA/qa.py
+++ blijax_backend/QA/qa.py
@@ -1,8 +1,11 @@
+#Imports
 import os
 import sys
-
-#import dotenv
-from dotenv import load_dotenv
+import codecs
+import requests
+import nltk
+import string
+import random
 
 #langchain
 from langchain.document_loaders import TextLoader
@@ -10,41 +13,71 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.document_loaders import SeleniumURLLoader
+from bs4 import BeautifulSoup
+from newspaper import Article
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+
+
+
+#import dotenv
+from dotenv import load_dotenv
 
 #loading dotenv
 load_dotenv()
 
+#openai API key from .env
 key = os.getenv("OPENAI_API_KEY")
 
-#openai API key from .env
 
 
-#query set to terminal answer
-# to run type this in terminal:  python3 Blijax-Stockbot/blijax_backend/QA/qa.py "Sum up the information into a couple sentences getting the key points"  
-# at the end of the command there is a section of text with apostrophes. That section is the prompt. Feel free to mess around with the prompt. 
-query = sys.argv[1]
 
 
-#loading the TXT data into 
-loader = TextLoader('Blijax-Stockbot/blijax_backend/QA/qaTEXT.txt')
+#Extracts URL
+def extract_text_from(url):
+     html = requests.get(url).text
+     soup = BeautifulSoup(html, features="html.parser")
+     text = soup.get_text()
 
-#Storing the vector index with the data through the loader
+     lines = (line.strip() for line in text.splitlines())
+     return '\n'.join(line for line in lines if line)
+
+#Sets text to all info
+text = extract_text_from("https://en.wikipedia.org/wiki/Quantum_mechanics")
+
+
+
+#def urlSummarizer(text): 
+ # initializing size of string
+N = 10
+ 
+ # using random.choices()
+ # generating random strings
+res = ''.join(random.choices(string.ascii_uppercase +
+                              string.digits, k=N))
+
+f = open(res + ".txt", "x")
+
+with open(res + '.txt', 'w') as f:
+  f.write(repr(text) + '\n')     
+
+
+
+ #Sets file to loader
+loader = TextLoader(res + ".txt")
+
+ #Storing the vector index with the data through the loader
 index = VectorstoreIndexCreator().from_loaders([loader])
 
-#printing the answer
-print(index.query(query))
+ #printing the answer
+print(index.query("Paraphrase this into a couple key points in a minium of five sentence"))
 
-
-
-# || Below is info for implementing into other code or frontend ||
-
-# do
-# import qa.py
-# and to set variables do qa.variableName 
-# example: qa.loader = TextLoader('qaTEXT.txt') || This sets the loader with the data to be used
-
-#when using this you can change it to implement to other things by setting the fully nested query in print on line 29 to "Sum up the information into a couple sentences getting the key points"
-#Then you should be able to set the loader variable, index variable, and then run line 29 to get the output
-
-# once this is done you can add a way to change/add to the qaTEXT by doing a with open function on the txt file from where you are calling/using qa.py 
-# after that is done you should be able to access the txt file and run the code to recieve an output
+ #removes the file
+os.remove(res + ".txt")
