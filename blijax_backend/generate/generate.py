4537719db9301cb4fe900a5be2d338c25ce6c606
Aaron <aaron.nguy2008@gmail.com>
2023-08-08 11:58:47 -0700

    Object oriented approach

--- blijax_backend/generate/generate.py
+++ blijax_backend/generate/generate.py
@@ -39,8 +39,6 @@ import requests
 import yfinance as yf
 import json
 
-from bs4 import BeautifulSoup
-
 from QA.qa import urlSummarizer
 
 # --- Environment variables --- #
@@ -49,215 +47,230 @@ load_dotenv()
 key = os.getenv("OPENAI_API_KEY")
 serpkey = os.getenv("SERPAPI_API_KEY")
 apilayerkey = os.getenv("API_LAYER_API_KEY")
-
-class blijax():
-    def __init__(self):
-        pass
-
-
-# --- Output format for stock outputs --- #
-json_schema = {
-    "title": "Company",
-    "description": "Information about the company",
-    "type": "object",
-    "properties": {
-        "name": {"title": "Name", "description": "The company's name", "type": "string"},
-        "news": {"title": "News", "description": "The recent news about the company", "type": "string"},
-        "stockPrice": {"title": "Price", "description": "The company's stock price"},
-        "recommend": {"title": "Recommend", "description": "If it is recommended to buy this stock"}
-    }
-}
-
-# --- Output format for news outputs --- #
-json_news_schema = {
-    "title": "Company",
-    "description": "Information about the company",
-    "type": "object",
-    "properties": {
-        "name": {"title": "Name", "description": "The company's name", "type": "string"},
-        "news": {"title": "News", "description": "The recent news about the company", "type": "string"},
-        "url": {"title": "Url", "description": "The url from the source"},
-    }
-}
-
-# --- Setting up language model --- #
-llm = ChatOpenAI(model="gpt-4", openai_api_key=key)
-
-# --- Loads serpapi tool, sets the language model to use, and sets the api key --- #
-tools = load_tools(["serpapi"], llm=llm, serpapi_api_key=serpkey)
-
-search = SerpAPIWrapper()
-
-# --- Define a list of tools offered by the agent --- #
-tools = [
-    Tool(
-        name="Search",
-        func=search.run,
-        description="Useful when you need to answer questions about current events. You should ask targeted questions.",
-    ),
-]
-
-agent = initialize_agent(
-    tools, llm, agent=AgentType.OPENAI_MULTI_FUNCTIONS, verbose=True
-)
-
-# --- Prompt for extracting news sources --- #
-prompt_msgs = [
-    SystemMessage(
-        content="You are a world class algorithm for extracting new information about large companies."
-    ),
-    HumanMessage(
-        content="Extract the name of the company, news title, and the source url from the following input:"
-    ),
-    HumanMessagePromptTemplate.from_template("{input}"),
-    HumanMessage(content="Tips: Make sure to answer in the correct format"),
-]
-
-# --- Prompt for extraction yahoo finance information --- #
-messages = [
-    SystemMessage(
-        content="You are a world class algorithm for extraction information from JSON format."
-    ),
-    HumanMessage(
-        content="Extract the company name, stock prices, and if it is recommended to buy from the following input:"
-    ),
-    HumanMessagePromptTemplate.from_template("{input}"),
-    HumanMessage(
-        content="Tips: Make sure to answer in the correct format."
-    )
-]
-
-# --- Extracts text from a url, useful for summarize news --- #
-def extract_text_from(url):
-    html = requests.get(url).text
-    soup = BeautifulSoup(html, features="html.parser")
-    text = soup.get_text()
-
-    lines = (line.strip() for line in text.splitlines())
-    return '\n'.join(line for line in lines if line)
-
-
-# --- Retrieves news --- #
-def retrieveNews(input):
-    prompt = ChatPromptTemplate(messages=prompt_msgs)
-    url = f"https://api.apilayer.com/financelayer/news?tickers={input}"
-
-    payload = {}
-    headers= {
-        "apikey": apilayerkey
-    }
-
-    response = requests.request("GET", url, headers=headers, data = payload)
-
-    result = response.text
-
-    chain = create_structured_output_chain(json_news_schema, llm, prompt, verbose=True)
-    jsonResponse = chain.run(json.dumps(result))
-    
-    #return jsonResponse
-    
-    urls = [str(jsonResponse["url"])]
-    returned = urlSummarizer(urls)
-
-    return returned
-    
-    
-# --- Retrieves Stock Prices --- #
+"""
+def retrieveNews(company_name: str):
+    print("news")
 def retrieveStocks(company_name: str):
-    prompt = ChatPromptTemplate(messages=messages)
-    comp = yf.Ticker(company_name)
+    print("stocks")
+def questionsAboutCurrent(input: str):
+    print("questions")
+def generalConversation(input: str):
+    print("conversation")
+"""
+class Blijax:
+    def __init__(self, ai_model: str, summarize_length: str):
+        self.ai_model = ai_model
+        self.summarize_length = summarize_length
+        self.llm = ChatOpenAI(model=ai_model, openai_api_key=key)
+        self.search = SerpAPIWrapper()
+        self.tools = load_tools(["serpapi"], llm=self.llm, serpapi_api_key=serpkey)
+        self.tools = [
+            Tool(
+                name="Search",
+                func=self.search.run,
+                description="Useful when you need to answer questions about current events. You should ask targeted questions.",
+            ),
+        ]
+        self.agent = initialize_agent(
+            self.tools, self.llm, agent=AgentType.OPENAI_MULTI_FUNCTIONS, verbose=True
+        )
 
-    chain = create_structured_output_chain(json_schema, llm, prompt, verbose=True)
-    return chain.run(json.dumps(comp.info))
+        self.json_schema = {
+            "title": "Company",
+            "description": "Information about the company",
+            "type": "object",
+            "properties": {
+                "name": {"title": "Name", "description": "The company's name", "type": "string"},
+                "news": {"title": "News", "description": "The recent news about the company", "type": "string"},
+                "stockPrice": {"title": "Price", "description": "The company's stock price"},
+                "recommend": {"title": "Recommend", "description": "If it is recommended to buy this stock"}
+            }
+        }
 
-# --- Identifies ticker in text --- #
-def retrieveTicker(input):
+            # --- Output format for news outputs --- #
+        self.json_news_schema = {
+            "title": "Company",
+            "description": "Information about the company",
+            "type": "object",
+            "properties": {
+                "name": {"title": "Name", "description": "The company's name", "type": "string"},
+                "news": {"title": "News", "description": "The recent news about the company", "type": "string"},
+                "url": {"title": "Url", "description": "The url from the source"},
+            }
+        }
+
+        self.news_messages = [
+            SystemMessage(
+                content="You are a world class algorithm for extracting new information about large companies."
+            ),
+            HumanMessage(
+                content="Extract the name of the company, news title, and the source url from the following input:"
+            ),
+            HumanMessagePromptTemplate.from_template("{input}"),
+            HumanMessage(content="Tips: Make sure to answer in the correct format"),
+        ]
+
+        # --- Prompt for extraction yahoo finance information --- #
+        self.stock_messages = [
+            SystemMessage(
+                content="You are a world class algorithm for extraction information from JSON format."
+            ),
+            HumanMessage(
+                content="Extract the company name, stock prices, and if it is recommended to buy from the following input:"
+            ),
+            HumanMessagePromptTemplate.from_template("{input}"),
+            HumanMessage(
+                content="Tips: Make sure to answer in the correct format."
+            )
+        ]
+
+        stock_prompt = ChatPromptTemplate(messages=self.stock_messages)
+        news_prompt = ChatPromptTemplate(messages=self.news_messages)
+
+        self.stock_chain = create_structured_output_chain(self.json_schema, self.llm, stock_prompt, verbose=True)
+        self.news_chain = create_structured_output_chain(self.json_schema, self.llm, news_prompt, verbose=True)
+        #self.decision_chain = None
+        msgs = [
+            SystemMessage(
+                content="You are a helpful assistant who retrieves the latest information about large companies."
+            ),
+            HumanMessage(
+                content="Make calls to the relevant function to record information in the following input:"
+            ),
+            HumanMessagePromptTemplate.from_template("{input}"),
+            HumanMessage(content="Tips: Make sure to answer in the correct format.")
+        ]
+        prompt = ChatPromptTemplate(messages=msgs)
+        self.decision_chain = create_openai_fn_chain([self.retrieveNews, self.retrieveStocks, self.questionsAboutCurrent, self.generalConversation], self.llm, prompt, verbose=True)
+
+    # --- Output format for stock outputs --- #
+    
+
+    # --- Setting up language model --- #
+    
+
+    # --- Loads serpapi tool, sets the language model to use, and sets the api key --- #
+    
+    
+
+    # --- Define a list of tools offered by the agent --- #
+    
+
+    
+
+    # --- Prompt for extracting news sources --- #
+
+    # --- Retrieves news --- #
+    def retrieveNews(self, company_ticker: str) -> str:
+        url = f"https://api.apilayer.com/financelayer/news?tickers={company_ticker}"
+
+        payload = {}
+        headers= {
+            "apikey": apilayerkey
+        }
+
+        response = requests.request("GET", url, headers=headers, data = payload)
+
+        result = response.text
+
+        jsonResponse = self.news_chain.run(json.dumps(result))
+        
+        #return jsonResponse
+        
+        urls = [str(jsonResponse["url"])]
+        returned = urlSummarizer(urls, self.summarize_length)
+
+        return returned
+        
+    # --- Retrieves Stock Prices --- #
+    def retrieveStocks(self, company_name: str) -> str:
+        comp = yf.Ticker(company_name)
+        return self.stock_chain.run(json.dumps(comp.info))
+
+    # --- Identifies ticker in text --- #
+    def retrieveTicker(self, input):
+        msgs = [
+            SystemMessage(
+                content="You are a helpful assistant who retrieves the company ticker from a string of text."
+            ),
+            HumanMessage(
+                content="Extract the company ticker from the following input:"
+            ),
+            HumanMessagePromptTemplate.from_template("{input}"),
+            HumanMessage(content="Tips: Make sure to answer in the correct format")
+        ]
+
+        prompt = ChatPromptTemplate(messages=msgs)
+        json_schema = {
+            "title": "Company",
+            "description": "Company Ticker",
+            "type": "object",
+            "properties": {
+                "ticker": {"title": "ticker", "description": "The company's ticker", "type": "string"},
+            }
+        }
+
+        chain = create_structured_output_chain(json_schema, self.llm, prompt, verbose=True)
+        return chain.run(input)
+
+    
+    def questionsAboutCurrent(self, input) -> str:
+       
+        return self.agent.run(input)
+
+    def generalConversation(self, input) -> str:
+        
+        return self.llm.predict(input)
+    
+
+    # --- Decision prompt; helps LLM decide which function to call --- #
     msgs = [
         SystemMessage(
-            content="You are a helpful assistant who retrieves the company ticker from a string of text."
+            content="You are a helpful assistant who retrieves the latest information about large companies."
         ),
         HumanMessage(
-            content="Extract the company ticker from the following input:"
+            content="Make calls to the relevant function to record information in the following input:"
         ),
         HumanMessagePromptTemplate.from_template("{input}"),
-        HumanMessage(content="Tips: Make sure to answer in the correct format")
+        HumanMessage(content="Tips: Make sure to answer in the correct format.")
     ]
 
-    prompt = ChatPromptTemplate(messages=msgs)
-    json_schema = {
-        "title": "Company",
-        "description": "Company Ticker",
-        "type": "object",
-        "properties": {
-            "ticker": {"title": "ticker", "description": "The company's ticker", "type": "string"},
-        }
-    }
+    # --- Generates Response --- #
+    def generate(self, text):
+        #prompt = ChatPromptTemplate(messages=self.msgs)
 
-    chain = create_structured_output_chain(json_schema, llm, prompt, verbose=True)
-    return chain.run(input)
-
-def extract_text_from(url):
-    html = requests.get(url).text
-    soup = BeautifulSoup(html, features="html.parser")
-    text = soup.get_text()
-
-    lines = (line.strip() for line in text.splitlines())
-    return '\n'.join(line for line in lines if line)
- 
-def questionsAboutCurrent(input):
-    """
-    Questions which are unrelated to finance.
-    """
-    return agent.run(input)
-
-def generalConversation(input):
-    """
-    General conversation
-    """
-    return llm.predict(input)
-
-# --- Decision prompt; helps LLM decide which function to call --- #
-msgs = [
-    SystemMessage(
-        content="You are a helpful assistant who retrieves the latest information about large companies."
-    ),
-    HumanMessage(
-        content="Make calls to the relevant function to record information in the following input:"
-    ),
-    HumanMessagePromptTemplate.from_template("{input}"),
-    HumanMessage(content="Tips: Make sure to answer in the correct format")
-]
-
-# --- Generates Response --- #
-def generate(text):
-    prompt = ChatPromptTemplate(messages=msgs)
-
-    input = text
-
-    chain = create_openai_fn_chain([retrieveNews, retrieveStocks, questionsAboutCurrent, generalConversation], llm, prompt, verbose=True)
-    decision = chain.run(input)
-    print(decision)
-
-
-    if (decision["name"] == "retrieveNews"):
-        ticker = retrieveTicker(input)
+        input = text
+        #[self.retrieveNews, self.retrieveStocks, self.questionsAboutCurrent, self.generalConversation]
+        #[retrieveNews, retrieveStocks, questionsAboutCurrent, generalConversation]
         
-        newsReply = retrieveNews(ticker["ticker"])
-        print(newsReply)
+
+        if not self.decision_chain:
+            pass
+            
+        decision = self.decision_chain.run(input)
+        print(decision)
         
-        return newsReply
-    
-    elif decision["name"] == "retrieveStocks":
+        if (decision["name"] == "retrieveNews"):
+            ticker = self.retrieveTicker(input)
+            
+            newsReply = self.retrieveNews(ticker["ticker"])
+            print(newsReply)
+            
+            return newsReply
         
-        tickerReply = retrieveStocks(decision["arguments"]["company_name"])
-        return llm.predict(f"Can you summarize this?: {tickerReply}")
-    
-    elif decision["name"] == "questionsAboutCurrent":
+        elif decision["name"] == "retrieveStocks":
+            
+            tickerReply = self.retrieveStocks(decision["arguments"]["company_name"])
+            return self.llm.predict(f"Can you summarize this?: {tickerReply}")
         
-        return questionsAboutCurrent(input)
-    
-    elif decision["name"] == "generalConversation":
+        elif decision["name"] == "questionsAboutCurrent":
+            
+            return self.questionsAboutCurrent(input)
         
-        return generalConversation(input)
-    
-#print(generate("Should I buy msft stock?"))
+        elif decision["name"] == "generalConversation":
+            
+            return self.generalConversation(input)
+        
+blijax_model = Blijax("gpt-4", "in 5 sentences")
+#print(blijax_model.generate("What's the stock price of aapl?"))
+#print(blijax_model.retrieveNews.__doc__)
