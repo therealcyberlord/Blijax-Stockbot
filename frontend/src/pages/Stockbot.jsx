29dc8522700ea1a67fed3dcbd1ad4716fc15e165
Xingyu Bian <xingyubian@umass.edu>
2023-08-09 18:44:45 -0400

    role added

--- frontend/src/pages/Stockbot.jsx
+++ frontend/src/pages/Stockbot.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const getBot = (prompt) => {
   const url = "http://localhost:8000" + "/generate/";
--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -1,46 +1,70 @@
-import React from 'react';
-
 const Team = () => {
   return (
     <div className="container mx-auto mt-8">
       <div className="flex justify-center">
         <div className="flex flex-col items-center">
           <div className="bg-blue-400 p-6 rounded-full shadow-lg shadow-blue-600 transform transition-all hover:scale-105 mb-4">
-            <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 1" className="w-full rounded-full" />
+            <img
+              src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+              alt="Image 1"
+              className="w-full rounded-full"
+            />
           </div>
-          <h3 className="text-xl font-bold mb-2">Blake Almon</h3>
+          <h3 className="text-xl font-bold mb-2">Blake Almon - Backend</h3>
         </div>
         <div className="flex flex-col items-center">
           <div className="bg-blue-400 p-6 rounded-full shadow-lg shadow-blue-600 transform transition-all hover:scale-105 mb-4">
-            <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 2" className="w-full rounded-full" />
+            <img
+              src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+              alt="Image 2"
+              className="w-full rounded-full"
+            />
           </div>
-          <h3 className="text-xl font-bold mb-2">Aaron Nguy</h3>
+          <h3 className="text-xl font-bold mb-2">Aaron Nguy - Backend</h3>
         </div>
         <div className="flex flex-col items-center">
           <div className="bg-blue-400 p-6 rounded-full shadow-lg shadow-blue-600 transform transition-all hover:scale-105 mb-4">
-            <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 3" className="w-full rounded-full" />
+            <img
+              src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+              alt="Image 3"
+              className="w-full rounded-full"
+            />
           </div>
-          <h3 className="text-xl font-bold mb-2">Sophia Lee</h3>
+          <h3 className="text-xl font-bold mb-2">Sophia Lee - Frontend</h3>
         </div>
         <div className="flex flex-col items-center">
           <div className="bg-blue-400 p-6 rounded-full shadow-lg shadow-blue-600 transform transition-all hover:scale-105 mb-4">
-            <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 4" className="w-full rounded-full" />
+            <img
+              src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+              alt="Image 4"
+              className="w-full rounded-full"
+            />
           </div>
-          <h3 className="text-xl font-bold mb-2">Isaac Jung</h3>
+          <h3 className="text-xl font-bold mb-2">Isaac Jung - Backend</h3>
         </div>
         <div className="flex flex-col items-center">
           <div className="bg-blue-400 p-6 rounded-full shadow-lg shadow-blue-600 transform transition-all hover:scale-105 mb-4">
-            <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 5" className="w-full rounded-full" />
+            <img
+              src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+              alt="Image 5"
+              className="w-full rounded-full"
+            />
           </div>
-          <h3 className="text-xl font-bold mb-2">Jackson Pirooz</h3>
+          <h3 className="text-xl font-bold mb-2">Jackson Pirooz - Frontend</h3>
         </div>
       </div>
       <div className="flex justify-center">
         <div className="flex flex-col items-center">
           <div className="bg-blue-400 p-8 rounded-full shadow-lg shadow-blue-600 transform transition-all hover:scale-105 mb-4">
-            <img src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg" alt="Image 6" className="w-full rounded-full" />
+            <img
+              src="https://t3.ftcdn.net/jpg/01/97/11/64/360_F_197116416_hpfTtXSoJMvMqU99n6hGP4xX0ejYa4M7.jpg"
+              alt="Image 6"
+              className="w-full rounded-full"
+            />
           </div>
-          <h3 className="text-xl font-bold mb-2">Xingyu Bian</h3>
+          <h3 className="text-xl font-bold mb-2">
+            Xingyu Bian - Product Manager
+          </h3>
         </div>
       </div>
     </div>
