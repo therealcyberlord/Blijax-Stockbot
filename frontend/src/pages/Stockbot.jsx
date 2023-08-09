fef27382c19b511a0de0950b994347fb36674ce4
Xingyu Bian <xingyubian@umass.edu>
2023-08-09 14:22:06 -0400

    added chat-like interface

--- frontend/src/pages/Stockbot.jsx
+++ frontend/src/pages/Stockbot.jsx
@@ -1,48 +1,78 @@
-import { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const getBot = (prompt) => {
   const url = "http://localhost:8000" + "/generate/";
   const options = {
     method: "POST",
     headers: {
-      "Content-Type": "application/json", // this part is important!
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(
-      // must JSON encode request body
-      {
-        text: prompt,
-      }
-    ),
+    body: JSON.stringify({
+      text: prompt,
+    }),
   };
-  return fetch(url, options).then((res) => res.json()); // returns a Promise
+  return fetch(url, options).then((res) => res.json());
 };
 
 const Stockbot = () => {
-  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState([]);
+  const inputRef = useRef(null);
 
   const handleInputChange = (event) => {
     if (event.keyCode === 13) {
-      // Check if Enter key is pressed
-      getBot(event.target.value)
+      const newMessage = event.target.value;
+      const newMessages = [...messages, { text: newMessage, isUser: true }];
+      setMessages(newMessages);
+
+      getBot(newMessage)
         .then((result) => {
-          setInputValue(result);
+          const botResponse = result;
+          const newMessagesWithResponse = [
+            ...newMessages,
+            { text: botResponse, isUser: false },
+          ];
+          setMessages(newMessagesWithResponse);
         })
         .catch((error) => {
-          setInputValue(error.message);
+          const errorMessage = error.message;
+          const newMessagesWithError = [
+            ...newMessages,
+            { text: errorMessage, isUser: false },
+          ];
+          setMessages(newMessagesWithError);
         });
-      event.target.value = ""; // Clear the text box after Enter is pressed
+
+      event.target.value = "";
     }
   };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="absolute top-24 bg-blue-300 p-6 rounded-lg w-5/6 h-2/3">
-        <div className="bg-blue-300 p-6 rounded-lg w-full h-full">
-          <h3>{inputValue}</h3>
+        <div className="bg-blue-300 p-6 rounded-lg w-full h-full overflow-y-scroll">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`mb-2 ${message.isUser ? "text-right" : "text-left"}`}
+            >
+              <div
+                className={`${
+                  message.isUser ? "bg-blue-500 text-white" : "bg-white"
+                } inline-block px-4 py-2 rounded-lg`}
+              >
+                {message.text}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <div className="absolute bottom-4 bg-blue-300 p-6 rounded-lg w-5/6">
         <input
+          ref={inputRef}
           type="text"
           className="w-full bg-white border border-blue-300 rounded px-4 py-2 mt-2"
           placeholder="Type here..."
