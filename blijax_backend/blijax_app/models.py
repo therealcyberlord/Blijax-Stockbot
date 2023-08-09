a2872e79ef14e214c128bf8f9152f3ade8442f5c
Xingyu Bian <xingyubian@umass.edu>
2023-08-09 08:54:51 -0400

    removed backend folder on frontend

--- blijax_backend/blijax_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.db import models
-
-class Text(models.Model):
-	text = models.CharField(max_length = 500, blank = False)
-	time_sent = models.DateTimeField(auto_now_add = True) 
